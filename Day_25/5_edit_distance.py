0de4317171452a8ffe8f2945c40f07c87685541c
Anoubhav Agarwaal <anoubhav.agarwaal@gmail.com>
2020-10-17 12:57:25 +0530

    added 1D dp for edit distance

--- Day_25/5_edit_distance.py
+++ Day_25/5_edit_distance.py
@@ -1,4 +1,5 @@
 def minDistance(word1, word2) -> int:
+    # O(N^2) time and space
     n, m = len(word1), len(word2)
     dp = [[0]*(m+1) for _ in range(n+1)]
     
@@ -19,6 +20,43 @@ def minDistance(word1, word2) -> int:
                 dp[i][j] = min(insert, delete, replace)
 
     return dp[n][m]
+
+def minDistance1DP(s, t):
+    # O(N^2) time and O(N) space
+    n, m = len(s), len(t)
+
+    prevrow = [0] * (m + 1)
+    for j in range(m + 1):
+        prevrow[j] = j
+    
+    for i in range(1, n + 1):
+        current = [0] * (m + 1)
+        current[0] = i
+
+        for j in range(1, m + 1):
+            if s[i - 1] == t[j - 1]:
+                current[j] = prevrow[j - 1]
+            else:
+                current[j] = min(prevrow[j - 1], current[j - 1], prevrow[j]) + 1
+        
+        prevrow = current[:]
+    
+    return prevrow[m]
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
 
 word1 = "zoologicoarchaeologist"
 word2 = "zoogeologist"
